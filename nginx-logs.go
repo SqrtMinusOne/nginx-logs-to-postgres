88489e653d47f4f13de600cbbd16df55cc2a4885
SqrtMinusOne <thexcloud@gmail.com>
2021-02-22 14:20:21 +0300

    refactor: use inline template instead of file

--- nginx-logs.go
+++ nginx-logs.go
@@ -149,8 +149,34 @@ func processLogs(conn *pgx.Conn) {
 	}
 }
 
+var createTemplate = `
+create table if not exists "{{.Schema}}"."{{.Table}}"
+(
+	id serial
+		constraint nginx_pk
+			primary key,
+	time_local varchar(256),
+	path text,
+	ip varchar(256),
+	remote_user varchar(256),
+	remote_port int,
+    time timestamp,
+	user_agent text,
+	user_id_got text,
+	user_id_set text,
+	request text,
+	status int,
+	body_bytes_sent int,
+	request_time float,
+	request_method varchar(128),
+	server_name varchar(256),
+	geoip_country_code varchar(256),
+	http_referrer text
+);
+`
+
 func initDb(conn *pgx.Conn) {
-	tmpl, err := template.ParseFiles("nginx-table.sql")
+	tmpl, err := template.New("nginx-table.sql").Parse(createTemplate)
 	if err != nil {
 		panic(err)
 	}
